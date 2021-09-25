aece96b608ab158e10def697aa2bc5134389f475
shashank <shankybansalymca@gmail.com>
2021-09-25 18:10:20 +0530

    learning git-limit service

---
+++ src/main/java/com/in28minutes/microservices/limitsservice/configuration/Configuration.java
@@ -0,0 +1,35 @@
+package com.in28minutes.microservices.limitsservice.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@ConfigurationProperties("limit-service")
+public class Configuration {
+	
+	private int minimum;
+	private int maximum;
+	private int average;
+	public int getMinimum() {
+		return minimum;
+	}
+	public void setMinimum(int minimum) {
+		this.minimum = minimum;
+	}
+	public int getMaximum() {
+		return maximum;
+	}
+	public void setMaximum(int maximum) {
+		this.maximum = maximum;
+	}
+	public int getAverage() {
+		return average;
+	}
+	public void setAverage(int average) {
+		this.average = average;
+	}
+	
+	
+
+}
